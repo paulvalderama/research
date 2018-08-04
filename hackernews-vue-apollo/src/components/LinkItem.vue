fbe8fa32d25ebd12c8d3bce637c64402a8f098f5
Erik Cox <erikbcox@gmail.com>
2018-08-04 12:01:57 -0700

    Add chrome devtool skeleton

--- hackernews-vue-apollo/src/components/LinkItem.vue
+++ hackernews-vue-apollo/src/components/LinkItem.vue
@@ -14,36 +14,36 @@
 </template>
 
 <script>
-import { timeDifferenceForDate } from '../utils'
-import { GC_USER_ID, GC_USER_NAME } from '../constants/settings'
-import { ALL_LINKS_QUERY, CREATE_VOTE_MUTATION } from '../constants/graphql'
+import { timeDifferenceForDate } from "../utils";
+import { GC_USER_ID, GC_USER_NAME } from "../constants/settings";
+import { ALL_LINKS_QUERY, CREATE_VOTE_MUTATION } from "../constants/graphql";
 
 export default {
-  name: 'LinkItem',
+  name: "LinkItem",
   computed: {
-    userId () {
-      return this.$root.$data.userId
+    userId() {
+      return this.$root.$data.userId;
     },
-    linkNumber () {
-      if (this.$route.path.includes('new')) {
-        return (this.pageNumber - 1) * this.linksPerPage + (this.index + 1)
+    linkNumber() {
+      if (this.$route.path.includes("new")) {
+        return (this.pageNumber - 1) * this.linksPerPage + (this.index + 1);
       } else {
-        return this.index + 1
+        return this.index + 1;
       }
     }
   },
-  props: ['link', 'index'],
+  props: ["link", "index"],
   methods: {
     timeDifferenceForDate,
-    voteForLink () {
-      const userId = localStorage.getItem(GC_USER_ID)
-      const voterIds = this.link.votes.map(vote => vote.user.id)
-      const userName = localStorage.getItem(GC_USER_NAME)
+    voteForLink() {
+      const userId = localStorage.getItem(GC_USER_ID);
+      const voterIds = this.link.votes.map(vote => vote.user.id);
+      const userName = localStorage.getItem(GC_USER_NAME);
       if (voterIds.includes(userId)) {
-        alert(`User (${userId}) already voted for this link.`)
-        return
+        alert(`User (${userId}) already voted for this link.`);
+        return;
       }
-      const linkId = this.link.id
+      const linkId = this.link.id;
       this.$apollo.mutate({
         mutation: CREATE_VOTE_MUTATION,
         variables: {
@@ -51,30 +51,45 @@ export default {
           linkId
         },
         update: (store, { data: { createVote } }) => {
-          this.updateStoreAfterVote(store, createVote, linkId)
+          this.updateStoreAfterVote(store, createVote, linkId);
+          var styles = [
+            "background: linear-gradient(#D33106, #571402)",
+            "border: 1px solid #3E0E02",
+            "display: block",
+            "text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)",
+            "box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset",
+            "line-height: 40px",
+            "text-align: center",
+            "font-weight: bold",
+            "font-size: 14px"
+          ].join(";");
+
+          console.log(
+            `%c new state change: +++++${userName} added a vote! ${data}`,
+            styles
+          );
         }
-      })
-      console.log(`new state change: +++++${userName} added a vote!`)
+      });
     },
-    updateStoreAfterVote (store, createVote, linkId) {
+    updateStoreAfterVote(store, createVote, linkId) {
       const data = store.readQuery({
         query: ALL_LINKS_QUERY
-      })
-      const votedLink = data.allLinks.find(link => link.id === linkId)
-      votedLink.votes = createVote.link.votes
-      store.writeQuery({ query: ALL_LINKS_QUERY, data })
+      });
+      const votedLink = data.allLinks.find(link => link.id === linkId);
+      votedLink.votes = createVote.link.votes;
+      store.writeQuery({ query: ALL_LINKS_QUERY, data });
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .upvote {
-    cursor: pointer;
-  }
+.upvote {
+  cursor: pointer;
+}
 
-  .link {
-    text-decoration: none;
-    color: black;
-  }
+.link {
+  text-decoration: none;
+  color: black;
+}
 </style>
