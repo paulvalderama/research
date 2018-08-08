8bc7d20780208eeeaa03a0d2fb8ebd9a6dfa7039
pvalderama <34151463+pvalderama@users.noreply.github.com>
2018-08-08 10:24:38 -0700

    paul contribution

--- hackernews-vue-apollo/src/components/LinkItem.vue
+++ hackernews-vue-apollo/src/components/LinkItem.vue
@@ -39,10 +39,10 @@ export default {
       const userId = localStorage.getItem(GC_USER_ID);
       const voterIds = this.link.votes.map(vote => vote.user.id);
       const userName = localStorage.getItem(GC_USER_NAME);
-      if (voterIds.includes(userId)) {
-        alert(`User (${userId}) already voted for this link.`);
-        return;
-      }
+      // if (voterIds.includes(userId)) {
+      //   alert(`User (${userId}) already voted for this link.`);
+      //   return;
+      // }
       const linkId = this.link.id;
       this.$apollo.mutate({
         mutation: CREATE_VOTE_MUTATION,
@@ -52,24 +52,8 @@ export default {
         },
         update: (store, { data: { createVote } }) => {
           this.updateStoreAfterVote(store, createVote, linkId);
-          var styles = [
-            "background: linear-gradient(#D33106, #571402)",
-            "border: 1px solid #3E0E02",
-            "display: block",
-            "text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)",
-            "box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset",
-            "line-height: 40px",
-            "text-align: center",
-            "font-weight: bold",
-            "font-size: 14px"
-          ].join(";");
-
-          console.log(
-            `%c new state change: +++++${userName} added a vote! ${data}`,
-            styles
-          );
         }
-      });
+      }).then(data => console.log(data.data))
     },
     updateStoreAfterVote(store, createVote, linkId) {
       const data = store.readQuery({
