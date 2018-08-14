401f3832e49a7a427feca677e181d46c3b9c9057
Erik Cox <erikbcox@gmail.com>
2018-08-14 11:32:59 -0700

    Add styling and update tool name

--- dev-tool/diff.js
+++ dev-tool/diff.js
@@ -1,21 +1,18 @@
 const observableDiff = require("deep-diff").observableDiff;
 const output = document.querySelector(".state-output");
 let counter = 0;
-
 chrome.devtools.panels.create(
-  "Apollo Time-Travel Debugger",
+  "Apollo State Debugger",
   "time_travel.png",
   "devtools.html",
   function(panel) {
-    output.innerHTML += `<p>Apollo time-travel debugger</p>`;
+    output.innerHTML += ``;
   }
 );
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.msg === "state_changed") {
     let lhs = request.data.state[0];
     let rhs = request.data.state[1];
-
     var entries = [];
     var additions = [];
     var edits = [];
@@ -23,56 +20,55 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     observableDiff(lhs, rhs, change => {
       switch (change.kind) {
         case "N":
-          additions.push(
-            `<p>Added: ${
-              change.path[change.path.length - 1]
-            }: <em>${JSON.stringify(change.rhs)}</em> </p>`
-          );
+          additions.push(`<p class="change"><span class="addition">Added:</span> ${
+            change.path[change.path.length - 1]
+          }
+        : <em> ${JSON.stringify(change.rhs)}
+        </em> </p>`);
           break;
-
         case "D":
-          deletions.push(`<p>Removed: <del>${
+          deletions.push(`<p><span class="deletion">Removed:</span> ${
             change.path[change.path.length - 1]
-          }: <em>${JSON.stringify(change.lhs)}</em></del>
-            </p>`);
+          } <del>
+        : <em>${JSON.stringify(change.lhs)}
+        </em></del> </p>`);
           break;
-
         case "E":
-          edits.push(`<p>Changed: <del>${
+          edits.push(`<p><span class="edit">Edited:</span> ${
             change.path[change.path.length - 1]
-          }: <em>${JSON.stringify(change.lhs)}</em></del> -> ${
+          }: <del><em>${JSON.stringify(
+            change.lhs
+          )}</em></del>   <strong>&rarr;</strong> ${
             change.path[change.path.length - 1]
-          }: <em>${JSON.stringify(change.rhs)}</em>
-              </p>`);
+          }
+        : <em>${JSON.stringify(change.rhs)}
+        </em> </p>`);
           break;
-        case "A":
-          // Don't output anything for the array case
+        case "A": // Don't output anything for the array case
           // output.innerHTML += `Change within array`;
           break;
       }
     });
-
     if (additions.length > 0)
-      entries.push(`<dd><details>
-      <summary>Additions</summary>
-      ${additions.join("")}
+      entries.push(`<dd><details> <summary class="change-type">Additions</summary> ${additions.join(
+        ""
+      )}
     </details></dd>`);
     if (edits.length > 0)
-      entries.push(`<dd><details>
-      <summary>Edits</summary>
-      ${edits.join("")}
+      entries.push(`<dd><details> <summary class="change-type">Edits</summary> ${edits.join(
+        ""
+      )}
     </details></dd>`);
     if (deletions.length > 0)
-      entries.push(`<dd><details>
-      <summary>Deletions</summary>
-      ${deletions.join("")}
+      entries.push(`<dd><details> <summary class="change-type">Deletions</summary> ${deletions.join(
+        ""
+      )}
     </details></dd>`);
+
     // Write html output here
-    const log = `<dl><details>
-        <summary>${counter}</summary>
-          <dt>${entries.join("")}</dt>
-      </details></dl>`;
-    console.log(log);
+    const log = `<dl><details> <summary class="mutation">${counter}
+    </summary> <dt>${entries.join("")}
+    </dt> </details></dl>`;
     output.innerHTML += log;
     counter += 1;
   }
