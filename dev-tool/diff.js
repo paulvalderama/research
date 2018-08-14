ad57eca3a89884987116c86d378a298eda33c0c1
Erik Cox <erikbcox@gmail.com>
2018-08-14 10:19:01 -0700

    Nest state change types

--- dev-tool/diff.js
+++ dev-tool/diff.js
@@ -1,7 +1,6 @@
 const observableDiff = require("deep-diff").observableDiff;
 const output = document.querySelector(".state-output");
 let counter = 0;
-let state = [];
 
 chrome.devtools.panels.create(
   "Apollo Time-Travel Debugger",
@@ -14,61 +13,67 @@ chrome.devtools.panels.create(
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.msg === "state_changed") {
-    state.push(request.data.state[0], request.data.state[1]);
-    let htmlObj = {};
+    let lhs = request.data.state[0];
+    let rhs = request.data.state[1];
 
-    if (state.length > 1) {
-      let lhs = state[state.length - 2];
-      let rhs = state[state.length - 1];
-      var prefix = "";
-      var middle = "";
-      var suffix = "";
-
-      observableDiff(lhs, rhs, change => {
-        switch (change.kind) {
-          case "N":
-            output.innerHTML += `<details> 
-        <summary>${counter}: New entry</summary>
-        <p>Added: ${change.path[change.path.length - 1]}: <em>${JSON.stringify(
-              change.rhs
-            )}</em> </p>
-          </details>`;
-            break;
-
-          case "D":
-            output.innerHTML += `<details> 
-        <summary>${counter}: Deletion</summary>
-        <p>Removed: <del>${
-          change.path[change.path.length - 1]
-        }: <em>${JSON.stringify(change.lhs)}</em></del>
-            </p>
-          </details>`;
-            break;
-
-          case "E":
-            prefix += `<details>
-          <summary>${counter}: Edit</summary>`;
-
-            middle += `<p>Changed: <del>${
+    var entries = [];
+    var additions = [];
+    var edits = [];
+    var deletions = [];
+    observableDiff(lhs, rhs, change => {
+      switch (change.kind) {
+        case "N":
+          additions.push(
+            `<p>Added: ${
               change.path[change.path.length - 1]
-            }: <em>${JSON.stringify(change.lhs)}</em></del> -> ${
-              change.path[change.path.length - 1]
-            }: <em>${JSON.stringify(change.rhs)}</em>
-              </p>`;
+            }: <em>${JSON.stringify(change.rhs)}</em> </p>`
+          );
+          break;
 
-            suffix += `</details>`;
+        case "D":
+          deletions.push(`<p>Removed: <del>${
+            change.path[change.path.length - 1]
+          }: <em>${JSON.stringify(change.lhs)}</em></del>
+            </p>`);
+          break;
 
-            break;
-          case "A":
-            // Don't output anything for the array case
-            // output.innerHTML += `Change within array`;
-            break;
-        }
-      });
-    }
-    // write output here
-    const wholeThing = `${prefix} ${middle} ${suffix}`;
-    if (counter > 0 && middle.length > 1) output.innerHTML += wholeThing;
+        case "E":
+          edits.push(`<p>Changed: <del>${
+            change.path[change.path.length - 1]
+          }: <em>${JSON.stringify(change.lhs)}</em></del> -> ${
+            change.path[change.path.length - 1]
+          }: <em>${JSON.stringify(change.rhs)}</em>
+              </p>`);
+          break;
+        case "A":
+          // Don't output anything for the array case
+          // output.innerHTML += `Change within array`;
+          break;
+      }
+    });
+
+    if (additions.length > 0)
+      entries.push(`<dd><details>
+      <summary>Additions</summary>
+      ${additions.join("")}
+    </details></dd>`);
+    if (edits.length > 0)
+      entries.push(`<dd><details>
+      <summary>Edits</summary>
+      ${edits.join("")}
+    </details></dd>`);
+    if (deletions.length > 0)
+      entries.push(`<dd><details>
+      <summary>Deletions</summary>
+      ${deletions.join("")}
+    </details></dd>`);
+    // Write html output here
+    const log = `<dl><details>
+        <summary>${counter}</summary>
+          <dt>${entries.join("")}</dt>
+      </details></dl>`;
+    console.log(log);
+    output.innerHTML += log;
     counter += 1;
   }
 });
